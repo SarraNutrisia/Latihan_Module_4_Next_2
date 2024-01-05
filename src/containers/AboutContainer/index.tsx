7ce7b6673dcb98729cc896a362b8f2e96af833db
Sarra Nutrisia <sarra.nutrisia@gmail.com>
2024-01-05 17:57:49 +0700

    Upload pertama

---
+++ src/containers/AboutContainer/index.tsx
@@ -0,0 +1,17 @@
+import { useSearchParams } from 'react-router-dom';
+
+const AboutContainer = () => {
+
+    const [searchParams] = useSearchParams();
+
+    console.log(searchParams.get('queryKey'))
+
+    return (
+        <div>
+            
+            Halaman About
+        </div>
+    )
+}
+
+export default AboutContainer
---
+++ src/containers/ContactContainer/index.tsx
@@ -0,0 +1,11 @@
+
+const ContactContainer = () => {
+
+    return (
+        <div>
+            Halaman Contact
+        </div>
+    )
+}
+
+export default ContactContainer
